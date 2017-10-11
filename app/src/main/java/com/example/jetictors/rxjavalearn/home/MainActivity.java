2e5896e98cebbc426d4fa66e53eb9fbed4a1e75c
Jetictors <zhengxcfutures@gmail.com>
2017-10-11 17:14:01 +0800

    引入RxJava库,并修改toolbar布局

---
+++ app/src/main/java/com/example/jetictors/rxjavalearn/home/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jetictors.rxjavalearn.home;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jetictors.rxjavalearn.R;
+
+/**
+ * 描述：首页
+ * 时间：2017/10/11  13:26
+ * 作者：Jetictors
+ * 版本  1.0.1
+ */
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/jetictors/rxjavalearn/index/GuideActivity.java
@@ -0,0 +1,10 @@
+package com.example.jetictors.rxjavalearn.index;
+
+/**
+ * 描述：引导页
+ * 时间：2017/10/11  13:24
+ * 作者：Jetictors
+ * 版本  1.0.1
+ */
+public class GuideActivity {
+}
